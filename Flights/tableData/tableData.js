1da2d4287e25df863bf39d27648d8f45610bc318
Vxt5074 <100242824+Vxt5074@users.noreply.github.com>
2022-04-20 17:46:28 -0400

    Add files via upload

---
+++ Flights/tableData/tableData.js
@@ -0,0 +1,152 @@
+var tbData = [
+    {
+        "ID": 1,
+        "Callsign": "N759CD",
+        "Departure": "PHL",
+        "Arrival": "AUS",
+        "dTime": "02:00:00",
+        "aTime": "05:00:00",
+        "Altitude": 28,
+        "speed": 220
+    },
+    {
+        "ID": 2,
+        "Callsign": "A475PG",
+        "Departure": "PHL",
+        "Arrival": "YYZ",
+        "dTime": "03:00:00",
+        "aTime": "05:00:00",
+        "Altitude": 30,
+        "speed": 200
+    },
+    {
+        "ID": 3,
+        "Callsign": "O3847LR",
+        "Departure": "PHL",
+        "Arrival": "ABY",
+        "dTime": "02:00:00",
+        "aTime": "08:30:00",
+        "Altitude": 30,
+        "speed": 230
+    },
+    {
+        "ID": 4,
+        "Callsign": "TWD23PS",
+        "Departure": "PHL",
+        "Arrival": "ALB",
+        "dTime": "11:00:00",
+        "aTime": "12:00:00",
+        "Altitude": 29,
+        "speed": 220
+    },
+    {
+        "ID": 5,
+        "Callsign": "T293R",
+        "Departure": "PHL",
+        "Arrival": "ATH",
+        "dTime": "12:00:00",
+        "aTime": "21:30:00",
+        "Altitude": 31,
+        "speed": 225
+    },
+    {
+        "ID": 6,
+        "Callsign": "EYU293W",
+        "Departure": "BDU",
+        "Arrival": "PHL",
+        "dTime": "09:00:00",
+        "aTime": "16:00:00",
+        "Altitude": 33,
+        "speed": 250
+    },
+    {
+        "ID": 7,
+        "Callsign": "TQ2123N",
+        "Departure": "BPT",
+        "Arrival": "PHL",
+        "dTime": "02:00:00",
+        "aTime": "18:00:00",
+        "Altitude": 34,
+        "speed": 300
+    },
+    {
+        "ID": 8,
+        "Callsign": "AH9238L",
+        "Departure": "BFL",
+        "Arrival": "PHL",
+        "dTime": "08:00:00",
+        "aTime": "19:00:00",
+        "Altitude": 38,
+        "speed": 265
+    },
+    {
+        "ID": 9,
+        "Callsign": "A923H",
+        "Departure": "BBA",
+        "Arrival": "PHL",
+        "dTime": "08:00:00",
+        "aTime": "10:00:00",
+        "Altitude": 27,
+        "speed": 230
+    },
+    {
+        "ID": 10,
+        "Callsign": "TH284N",
+        "Departure": "BKK",
+        "Arrival": "PHL",
+        "dTime": "10:30:00",
+        "aTime": "23:00:00",
+        "Altitude": 36,
+        "speed": 240
+    },
+    {
+        "ID": 11,
+        "Callsign": "WO0238R",
+        "Departure": "BKW",
+        "Arrival": "BEL",
+        "dTime": "01:00:00",
+        "aTime": "23:00:00",
+        "Altitude": 31,
+        "speed": 255
+    },
+    {
+        "ID": 12,
+        "Callsign": "W923T",
+        "Departure": "BRE",
+        "Arrival": "BJS",
+        "dTime": "11:30:00",
+        "aTime": "21:00:00",
+        "Altitude": 41,
+        "speed": 270
+    },
+    {
+        "ID": 13,
+        "Callsign": "Q2398Y",
+        "Departure": "BRO",
+        "Arrival": "BKX",
+        "dTime": "04:00:00",
+        "aTime": "10:00:00",
+        "Altitude": 40,
+        "speed": 260
+    },
+    {
+        "ID": 14,
+        "Callsign": "QP293H",
+        "Departure": "BDS",
+        "Arrival": "BOS",
+        "dTime": "05:00:00",
+        "aTime": "20:00:00",
+        "Altitude": 25,
+        "speed": 270
+    },
+    {
+        "ID": 15,
+        "Callsign": "UX203G",
+        "Departure": "CPR",
+        "Arrival": "BTV",
+        "dTime": "14:00:00",
+        "aTime": "18:00:00",
+        "Altitude": 44,
+        "speed": 300
+    }
+]
